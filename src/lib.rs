fe1873efb2dee7976876d2a13c18500b8428908f
strCarne <semionvoevoda@gmail.com>
2024-08-13 17:14:02 +0300

    refactor: Rename type aliases that ParsingResult depends

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 pub mod basic_types;
 pub mod primitives;
 
-pub use basic_types::*;
-
-type NextSeq<'a> = &'a str;
-type InputSeq<'a> = &'a str;
-type ParsingResult<'a, Output> = Result<(NextSeq<'a>, Output), InputSeq<'a>>;
+type NextInput<'a> = &'a str;
+type Input<'a> = &'a str;
+type ParsingResult<'a, Output> = Result<(NextInput<'a>, Output), Input<'a>>;
 
 pub trait Parser<'a, Output> {
     fn parse(&self, input: &'a str) -> ParsingResult<'a, Output>;
