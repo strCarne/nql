612dfa1d1b13299134d4610c230991db7d4e60a8
strCarne <semionvoevoda@gmail.com>
2024-08-14 14:12:03 +0300

    feat: Introduce new type BoxedParser for making indirections

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub mod basic_types;
-pub mod primitives;
 pub mod combinators;
+pub mod primitives;
 
 type NextInput<'a> = &'a str;
 type Input<'a> = &'a str;
@@ -16,5 +16,26 @@ where
 {
     fn parse(&self, input: &'a str) -> ParsingResult<'a, Output> {
         self(input)
+    }
+}
+
+pub struct BoxedParser<'a, Output> {
+    inner_parser: Box<dyn Parser<'a, Output> + 'a>,
+}
+
+impl<'a, Output> BoxedParser<'a, Output> {
+    pub fn new<P>(parser: P) -> Self
+    where
+        P: Parser<'a, Output> + 'a,
+    {
+        BoxedParser {
+            inner_parser: Box::new(parser),
+        }
+    }
+}
+
+impl<'a, Output> Parser<'a, Output> for BoxedParser<'a, Output> {
+    fn parse(&self, input: &'a str) -> ParsingResult<'a, Output> {
+        self.inner_parser.parse(input)
     }
 }
