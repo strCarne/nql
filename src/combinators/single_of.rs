1e7815b79a72c368de00bf24c2ec8a76d3efcc03
strCarne <semionvoevoda@gmail.com>
2024-08-14 10:34:29 +0300

    test: Make unit tests for single_of combinator

--- src/combinators/single_of.rs
+++ src/combinators/single_of.rs
@@ -16,8 +16,48 @@ pub fn single_of<'a, Output>(
 #[cfg(test)]
 mod tests {
 
+    use crate::primitives;
+
+    use super::*;
+    use pretty_assertions::assert_eq;
+
     #[test]
     fn single_of_combinator() {
-        todo!("Make unit test for combinators::single_of")
+        let input_data = vec![" ", "word", "fail"].into_iter();
+
+        let parsers = vec![
+            single_of(vec![
+                Box::new(primitives::literal("a")),
+                Box::new(primitives::literal("b")),
+                Box::new(primitives::literal(" ")),
+            ]),
+            single_of(vec![
+                Box::new(primitives::literal("or")),
+                Box::new(primitives::literal("wo")),
+                Box::new(primitives::literal("rd")),
+            ]),
+            single_of(vec![
+                Box::new(primitives::literal("i")),
+                Box::new(primitives::literal("i")),
+                Box::new(primitives::literal("l")),
+            ]),
+        ]
+        .into_iter();
+
+        let expected_results = vec![Ok(("", ())), Ok(("rd", ())), Err("fail")];
+
+        assert!(
+            input_data.len() == parsers.len() && parsers.len() == expected_results.len(),
+            "BAD TEST: number of inputs is not equal to number of results or parsers [correct the source data]"
+        );
+
+        let dataset = input_data
+            .zip(parsers)
+            .zip(expected_results)
+            .map(|((input, parser), expected)| (input, parser, expected));
+
+        for (input, parser, expected) in dataset {
+            assert_eq!(expected, parser.parse(input));
+        }
     }
 }
