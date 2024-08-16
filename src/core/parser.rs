49938e3fcd0b810746e47fa77d76eb54abe9fe75
strCarne <semionvoevoda@gmail.com>
2024-08-16 13:57:49 +0300

    refactor: Make Parser to return BoxedParser to add indirection

--- src/core/parser.rs
+++ src/core/parser.rs
@@ -8,23 +8,23 @@ pub type ParsingResult<'a, Output> = Result<(NextInput<'a>, Output), Input<'a>>;
 pub trait Parser<'a, Output> {
     fn parse(&self, input: &'a str) -> ParsingResult<'a, Output>;
 
-    fn map<F, NewOutput>(self, map_fn: F) -> impl Parser<'a, NewOutput>
+    fn map<F, NewOutput>(self, map_fn: F) -> BoxedParser<'a, NewOutput>
     where
         Self: Sized + 'a,
         Output: 'a,
         NewOutput: 'a,
         F: Fn(Output) -> NewOutput + 'a,
     {
-        combinators::map(self, map_fn)
+        combinators::map(self, map_fn).into_box()
     }
 
-    fn pred<F>(self, pred_fn: F) -> impl Parser<'a, Output>
+    fn pred<F>(self, pred_fn: F) -> BoxedParser<'a, Output>
     where
         Self: Sized + 'a,
         Output: 'a,
         F: Fn(&Output) -> bool + 'a,
     {
-        combinators::pred(self, pred_fn)
+        combinators::pred(self, pred_fn).into_box()
     }
 
     fn into_box(self) -> BoxedParser<'a, Output>
@@ -34,7 +34,7 @@ pub trait Parser<'a, Output> {
         BoxedParser::new(self)
     }
 
-    fn and_then<F, NextParser, NewOutput>(self, f: F) -> impl Parser<'a, NewOutput>
+    fn and_then<F, NextParser, NewOutput>(self, f: F) -> BoxedParser<'a, NewOutput>
     where
         Self: Sized + 'a,
         Output: 'a,
@@ -42,36 +42,36 @@ pub trait Parser<'a, Output> {
         NextParser: Parser<'a, NewOutput> + 'a,
         F: Fn(Output) -> NextParser + 'a,
     {
-        combinators::and_then(self, f)
+        combinators::and_then(self, f).into_box()
     }
 
-    fn wrap<Wrapper, WrapperOutput>(self, wrapper: Wrapper) -> impl Parser<'a, Output>
+    fn wrap<Wrapper, WrapperOutput>(self, wrapper: Wrapper) -> BoxedParser<'a, Output>
     where
         Self: Sized + 'a,
         Output: 'a,
         WrapperOutput: 'a,
         Wrapper: Parser<'a, WrapperOutput> + Clone + 'a,
     {
-        combinators::wrap(self, wrapper)
+        combinators::wrap(self, wrapper).into_box()
     }
 
-    fn wrap_before<Wrapper, WrapperOutput>(self, wrapper: Wrapper) -> impl Parser<'a, Output>
+    fn wrap_before<Wrapper, WrapperOutput>(self, wrapper: Wrapper) -> BoxedParser<'a, Output>
     where
         Self: Sized + 'a,
         Output: 'a,
         WrapperOutput: 'a,
         Wrapper: Parser<'a, WrapperOutput> + 'a,
     {
-        combinators::wrap_before(self, wrapper)
+        combinators::wrap_before(self, wrapper).into_box()
     }
 
-    fn wrap_after<Wrapper, WrapperOutput>(self, wrapper: Wrapper) -> impl Parser<'a, Output>
+    fn wrap_after<Wrapper, WrapperOutput>(self, wrapper: Wrapper) -> BoxedParser<'a, Output>
     where
         Self: Sized + 'a,
         Output: 'a,
         WrapperOutput: 'a,
         Wrapper: Parser<'a, WrapperOutput> + 'a,
     {
-        combinators::wrap_after(self, wrapper)
+        combinators::wrap_after(self, wrapper).into_box()
     }
 }
