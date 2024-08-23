acdf1c1834ac76e92198a9c61209c00b795c8827
strCarne <semionvoevoda@gmail.com>
2024-08-23 09:26:43 +0300

    feat: Make lang::to_lucene to convert lucene tokens into lucene lang

--- src/lang/mod.rs
+++ src/lang/mod.rs
@@ -6,3 +6,6 @@ pub use to_sql::to_sql;
 
 mod to_wql;
 pub use to_wql::to_wql;
+
+mod to_lucene;
+pub use to_lucene::to_lucene;
---
+++ src/lang/to_lucene.rs
@@ -0,0 +1,113 @@
+use crate::grammar::{
+    value::{Collection, Range, RangeOp, Value},
+    ComparasionOperator, Link, Statement,
+};
+
+use super::Token;
+
+pub fn to_lucene(tokens: &Vec<Token>) -> String {
+    let mut buf = String::new();
+    let mut prev_is_stmt = false;
+    let mut prev_is_link = false;
+
+    for token in tokens {
+        match token {
+            Token::Stmt(Statement::Field(field)) => {
+                if prev_is_stmt && !prev_is_link {
+                    buf.push_str(" AND ");
+                }
+                prev_is_stmt = true;
+                prev_is_link = false;
+
+                let op = match field.op {
+                    ComparasionOperator::Eq => ":",
+                    ComparasionOperator::Greater => ":>",
+                    ComparasionOperator::Less => ":<",
+                    ComparasionOperator::GreaterOrEq => ":>=",
+                    ComparasionOperator::LessOrEq => ":<=",
+                    ComparasionOperator::NotEq => ":",
+                };
+
+                match &field.v {
+                    Value::OrdinaryValue(val) => {
+                        if let ComparasionOperator::NotEq = field.op {
+                            buf.push_str("NOT ");
+                        }
+                        buf.push_str(&format!("{}{}{}", field.k, op, val.to_string()));
+                        buf.push(' ');
+                    }
+                    Value::Range(Range { bounds, op }) => {
+                        buf.push_str(&field.k);
+                        buf.push(':');
+                        let bounds = bounds.to_strings();
+                        let result = match op {
+                            RangeOp::EE => format!("{{{} TO {}}}", bounds.0, bounds.1),
+                            RangeOp::EI => format!("{{{} TO {}]", bounds.0, bounds.1),
+                            RangeOp::IE => format!("[{} TO {}}}", bounds.0, bounds.1),
+                            RangeOp::II => format!("[{} TO {}]", bounds.0, bounds.1),
+                        };
+                        buf.push_str(&result);
+                        buf.push(' ');
+                    }
+                    Value::Collection(Collection::AndColl(coll)) => {
+                        buf.push('(');
+
+                        let mut iter = coll.iter();
+                        if let Some(val) = iter.next() {
+                            buf.push_str(&format!("{}{}{}", field.k, op, val.to_string()));
+                        }
+
+                        for val in iter {
+                            buf.push_str(" AND ");
+                            buf.push_str(&format!("{}{}{}", field.k, op, val.to_string()));
+                        }
+                        buf.push(')');
+                        buf.push(' ');
+                        prev_is_stmt = true;
+                        prev_is_link = false;
+                    }
+                    Value::Collection(Collection::OrColl(coll)) => {
+                        buf.push('(');
+                        let mut iter = coll.iter();
+                        if let Some(val) = iter.next() {
+                            buf.push_str(&format!("{}{}{}", field.k, op, val.to_string()));
+                        }
+
+                        for val in iter {
+                            buf.push_str(" OR ");
+                            buf.push_str(&format!("{}{}{}", field.k, op, val.to_string()));
+                        }
+                        buf.push(')');
+                        buf.push(' ');
+                        prev_is_stmt = true;
+                        prev_is_link = false;
+                    }
+                }
+            }
+            Token::OpenBrace => {
+                if prev_is_stmt && !prev_is_link {
+                    buf.push_str("AND ");
+                }
+                prev_is_stmt = false;
+                prev_is_link = false;
+                buf.push('(');
+            }
+            Token::CloseBrace => {
+                prev_is_stmt = true;
+                prev_is_link = false;
+                buf.push(')');
+            }
+            Token::Link(link) => {
+                prev_is_stmt = false;
+                prev_is_link = true;
+                buf.push_str(match link {
+                    Link::And => " AND ",
+                    Link::Or => " OR ",
+                });
+            }
+            _ => continue,
+        }
+    }
+
+    buf
+}
