77c6e411a9165ceed0e63c7f67f67b1383869791
strCarne <semionvoevoda@gmail.com>
2024-08-27 14:16:55 +0300

    test: Implement unit test for grammar::and_coll

--- src/grammar/value/collection/and_coll.rs
+++ src/grammar/value/collection/and_coll.rs
@@ -14,9 +14,54 @@ pub fn and_coll(input: &str) -> ParsingResult<Vec<OrdinaryValue>> {
 #[cfg(test)]
 mod tests {
 
+    use super::*;
+    use crate::basic_types::Number;
+    use pretty_assertions::assert_eq;
+
     #[test]
     #[ignore = "not implemented yet"]
     fn and_coll_test() {
-        todo!("Make unit test")
+        let input_data = vec![
+            "{ 1, 2, 3, }",
+            "[ 1, 2, 3]",
+            "{1, 2, 3}",
+            "{mama,papa,ya,is,big,semya} next_input",
+        ]
+        .into_iter();
+
+        let expected_results = vec![
+            Err("{ 1, 2, 3, }"),
+            Err("[ 1, 2, 3]"),
+            Ok((
+                "",
+                vec![
+                    OrdinaryValue::Number(Number::Integer(1)),
+                    OrdinaryValue::Number(Number::Integer(2)),
+                    OrdinaryValue::Number(Number::Integer(3)),
+                ],
+            )),
+            Ok((
+                " next_input",
+                vec![
+                    OrdinaryValue::String(String::from("mama")),
+                    OrdinaryValue::String(String::from("papa")),
+                    OrdinaryValue::String(String::from("ya")),
+                    OrdinaryValue::String(String::from("is")),
+                    OrdinaryValue::String(String::from("big")),
+                    OrdinaryValue::String(String::from("semya")),
+                ],
+            )),
+        ]
+        .into_iter();
+
+        assert_eq!(
+            input_data.len(),
+            expected_results.len(),
+            "BAD TEST: number of inputs is not equal to number of results [correct the source data]"
+        );
+
+        for (input, expected) in input_data.zip(expected_results) {
+            assert_eq!(and_coll(input), expected);
+        }
     }
 }
