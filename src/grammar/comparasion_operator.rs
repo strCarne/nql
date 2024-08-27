bbbb9154c36a28317e80f5e06dfdb930307610a0
strCarne <semionvoevoda@gmail.com>
2024-08-27 10:16:29 +0300

    test: Implement unit test for grammar::comparasion_operator

--- src/grammar/comparasion_operator.rs
+++ src/grammar/comparasion_operator.rs
@@ -1,13 +1,13 @@
 use crate::{combinators::single_of, primitives::literal, Parser, ParsingResult};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum ComparasionOperator {
     Eq,
     NotEq,
     Less,
     Greater,
-    LessOrEq,
-    GreaterOrEq,
+    LessEq,
+    GreaterEq,
 }
 
 // COMPARSION_OPERATOR ::= '=' | '!=' | '<' | '>' | '<=' | '>='
@@ -15,8 +15,8 @@ pub fn comparasion_operator(input: &str) -> ParsingResult<ComparasionOperator> {
     single_of(vec![
         literal("!=").map(|_| ComparasionOperator::NotEq),
         literal("=").map(|_| ComparasionOperator::Eq),
-        literal("<=").map(|_| ComparasionOperator::LessOrEq),
-        literal(">=").map(|_| ComparasionOperator::GreaterOrEq),
+        literal("<=").map(|_| ComparasionOperator::LessEq),
+        literal(">=").map(|_| ComparasionOperator::GreaterEq),
         literal("<").map(|_| ComparasionOperator::Less),
         literal(">").map(|_| ComparasionOperator::Greater),
     ])
@@ -26,9 +26,52 @@ pub fn comparasion_operator(input: &str) -> ParsingResult<ComparasionOperator> {
 
 #[cfg(test)]
 mod tests {
+
+    use super::*;
+    use pretty_assertions::assert_eq;
+
     #[test]
-    #[ignore = "not implemented yet"]
     fn comparasion_operator_test() {
-        todo!("Make unit test")
+        use ComparasionOperator::*;
+
+        let input_data = vec![
+            " = ",
+            "<>",
+            "  aboba > boba ",
+            "=!",
+            "!=",
+            "f <=",
+            "<=",
+            "=<",
+            "=>",
+            ">=",
+            "",
+        ]
+        .into_iter();
+
+        let expected_results = vec![
+            Ok(("", Eq)),
+            Ok((">", Less)),
+            Err("aboba > boba "),
+            Ok(("!", Eq)),
+            Ok(("", NotEq)),
+            Err("f <="),
+            Ok(("", LessEq)),
+            Ok(("<", Eq)),
+            Ok((">", Eq)),
+            Ok(("", GreaterEq)),
+            Err(""),
+        ]
+        .into_iter();
+
+        assert_eq!(
+            input_data.len(),
+            expected_results.len(),
+            "BAD TEST: number of inputs is not equal to number of results [correct the source data]"
+        );
+
+        for (input, expected) in input_data.zip(expected_results) {
+            assert_eq!(comparasion_operator(input), expected);
+        }
     }
 }
