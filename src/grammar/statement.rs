db1717d33aa3b7fee5d81d3b77a9c87685f0f519
strCarne <semionvoevoda@gmail.com>
2024-08-27 13:46:22 +0300

    test: Implement unit test for grammar::statement

--- src/grammar/statement.rs
+++ src/grammar/statement.rs
@@ -19,9 +19,74 @@ pub fn statement(input: &str) -> ParsingResult<Statement> {
 
 #[cfg(test)]
 mod tests {
+
+    use crate::{
+        basic_types::Number,
+        grammar::{
+            value::{OrdinaryValue, Value},
+            ComparasionOperator,
+        },
+    };
+
+    use super::*;
+    use pretty_assertions::assert_eq;
+
     #[test]
-    #[ignore = "not implemented yet"]
     fn statement_test() {
-        todo!("Make unit test")
+        let input_data = vec![
+            "key =  value",
+            " key=value",
+            "$limit <> 10",
+            "limit != 10",
+            "$key = value",
+        ]
+        .into_iter();
+
+        let expected_results = vec![
+            Ok((
+                "",
+                Statement::Field(KeyValue {
+                    k: String::from("key"),
+                    op: ComparasionOperator::Eq,
+                    v: Value::OrdinaryValue(OrdinaryValue::String(String::from("value"))),
+                }),
+            )),
+            Err(" key=value"),
+            Ok((
+                " 10",
+                Statement::Extension(KeyValue {
+                    k: String::from("limit"),
+                    op: ComparasionOperator::Less,
+                    v: Value::OrdinaryValue(OrdinaryValue::String(String::from(">"))),
+                }),
+            )),
+            Ok((
+                "",
+                Statement::Field(KeyValue {
+                    k: String::from("limit"),
+                    op: ComparasionOperator::NotEq,
+                    v: Value::OrdinaryValue(OrdinaryValue::Number(Number::Integer(10))),
+                }),
+            )),
+            Ok((
+                "",
+                Statement::Extension(KeyValue {
+                    k: String::from("key"),
+                    op: ComparasionOperator::Eq,
+                    v: Value::OrdinaryValue(OrdinaryValue::String(String::from("value"))),
+                }),
+            )),
+        ]
+        .into_iter();
+
+        assert_eq!(
+            input_data.len(),
+            expected_results.len(),
+            "BAD TEST: number of inputs is not equal to number of results [correct the source data]"
+        );
+
+        for (input, expected) in input_data.zip(expected_results) {
+            assert_eq!(statement(input), expected);
+        }
     }
 }
