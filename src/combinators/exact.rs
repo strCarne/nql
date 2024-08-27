4dadc583cabaef6bf0379ddd9b7c78f18b6963b1
strCarne <semionvoevoda@gmail.com>
2024-08-27 15:24:18 +0300

    test: Implement unit test for combinators::exact

--- src/combinators/exact.rs
+++ src/combinators/exact.rs
@@ -21,9 +21,32 @@ where
 #[cfg(test)]
 mod tests {
 
+    use crate::primitives;
+
+    use super::*;
+    use pretty_assertions::assert_eq;
+
     #[test]
-    #[ignore = "not implemented yet"]
     fn exact_combinator() {
-        todo!("Make unit test")
+        let input_data = vec!["aaa", "aaaa", "aaaaa"].into_iter();
+
+        let parser = exact(primitives::character('a'), 4);
+
+        let expected_results = vec![
+            Err(""),
+            Ok(("", vec![(), (), (), ()])),
+            Ok(("a", vec![(), (), (), ()])),
+        ]
+        .into_iter();
+
+        assert_eq!(
+            input_data.len(),
+            expected_results.len(),
+            "BAD TEST: number of inputs is not equal to number of results [correct the source data]"
+        );
+
+        for (input, expected) in input_data.zip(expected_results) {
+            assert_eq!(parser.parse(input), expected);
+        }
     }
 }
