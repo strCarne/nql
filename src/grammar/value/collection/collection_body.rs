ea1cddf51eff974a3dd771be6f9d01cd96269177
strCarne <semionvoevoda@gmail.com>
2024-08-27 14:10:37 +0300

    test: Implement unit test for grammar::collection_body

--- src/grammar/value/collection/collection_body.rs
+++ src/grammar/value/collection/collection_body.rs
@@ -24,9 +24,62 @@ pub fn collection_body(mut input: &str) -> ParsingResult<Vec<OrdinaryValue>> {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use crate::basic_types::{Date, Number};
+    use pretty_assertions::assert_eq;
+
     #[test]
-    #[ignore = "not implemented yet"]
     fn collection_body_test() {
-        todo!("Make unit test")
+        let input_data = vec![
+            "1, 2 , 3",
+            "nice, 1, true, 01:01:2000 next_input",
+            " 1, 2, 3",
+            "1, 2, 3, ",
+        ]
+        .into_iter();
+
+        let expected_results = vec![
+            Ok((
+                "",
+                vec![
+                    OrdinaryValue::Number(Number::Integer(1)),
+                    OrdinaryValue::Number(Number::Integer(2)),
+                    OrdinaryValue::Number(Number::Integer(3)),
+                ],
+            )),
+            Ok((
+                " next_input",
+                vec![
+                    OrdinaryValue::String(String::from("nice")),
+                    OrdinaryValue::Number(Number::Integer(1)),
+                    OrdinaryValue::Boolean(true),
+                    OrdinaryValue::Date(Date {
+                        day: 1,
+                        month: 1,
+                        year: 2000,
+                    }),
+                ],
+            )),
+            Err(" 1, 2, 3"),
+            Ok((
+                ", ",
+                vec![
+                    OrdinaryValue::Number(Number::Integer(1)),
+                    OrdinaryValue::Number(Number::Integer(2)),
+                    OrdinaryValue::Number(Number::Integer(3)),
+                ],
+            )),
+        ]
+        .into_iter();
+
+        assert_eq!(
+            input_data.len(),
+            expected_results.len(),
+            "BAD TEST: number of inputs is not equal to number of results [correct the source data]"
+        );
+
+        for (input, expected) in input_data.zip(expected_results) {
+            assert_eq!(collection_body(input), expected);
+        }
     }
 }
