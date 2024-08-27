53d3881360d3732e8e6147f5c2cb006c37b3be98
strCarne <semionvoevoda@gmail.com>
2024-08-27 16:33:26 +0300

    test: Implement unit test for grammar::extension

--- src/grammar/extension.rs
+++ src/grammar/extension.rs
@@ -13,10 +13,45 @@ pub fn extension(input: &str) -> ParsingResult<KeyValue> {
 
 #[cfg(test)]
 mod tests {
+    use crate::grammar::{
+        extension,
+        value::{OrdinaryValue, Value},
+        ComparasionOperator, KeyValue,
+    };
 
     #[test]
-    #[ignore = "not implemented"]
     fn extension_test() {
-        
+        let input_data = vec!["$unique=true", " $unique=true", "$unique=true "].into_iter();
+
+        let expected_results = vec![
+            Ok((
+                "",
+                KeyValue {
+                    k: String::from("unique"),
+                    op: ComparasionOperator::Eq,
+                    v: Value::OrdinaryValue(OrdinaryValue::Boolean(true)),
+                },
+            )),
+            Err(" $unique=true"),
+            Ok((
+                "",
+                KeyValue {
+                    k: String::from("unique"),
+                    op: ComparasionOperator::Eq,
+                    v: Value::OrdinaryValue(OrdinaryValue::Boolean(true)),
+                },
+            )),
+        ]
+        .into_iter();
+
+        assert_eq!(
+            input_data.len(),
+            expected_results.len(),
+            "BAD TEST: number of inputs is not equal to number of results [correct the source data]"
+        );
+
+        for (input, expected) in input_data.zip(expected_results) {
+            assert_eq!(extension(input), expected);
+        }
     }
 }
