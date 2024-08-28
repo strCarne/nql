253594cd04be24d0864002b5b5150740cd4bed99
strCarne <semionvoevoda@gmail.com>
2024-08-28 09:14:55 +0300

    test: Implement unit test for lang::extract_extensions

--- src/lang/extract_extensions.rs
+++ src/lang/extract_extensions.rs
@@ -9,3 +9,48 @@ pub fn extract_extensions(mut tokens: NQLang) -> (NQLang, Vec<KeyValue>) {
     }
     (tokens, extensions)
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use crate::grammar::{KeyValue, NQToken, Unit};
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn extract_extensions_test() {
+        let input_data = vec![
+            vec![
+                NQToken::Unit(Unit::Stmt(KeyValue::default())),
+                NQToken::Extension(KeyValue::default()),
+                NQToken::Extension(KeyValue::default()),
+            ],
+            vec![
+                NQToken::Extension(KeyValue::default()),
+                NQToken::Extension(KeyValue::default()),
+            ],
+            vec![
+                NQToken::Unit(Unit::Stmt(KeyValue::default())),
+                NQToken::Unit(Unit::Stmt(KeyValue::default())),
+            ],
+            vec![],
+        ]
+        .into_iter();
+
+        let expected_results = vec![(1, 2), (0, 2), (2, 0), (0, 0)].into_iter();
+
+        assert_eq!(
+            input_data.len(),
+            expected_results.len(),
+            "BAD TEST: number of inputs is not equal to number of results [correct the source data]"
+        );
+
+        for (input, expected) in input_data.zip(expected_results) {
+            let result = {
+                let tmp = extract_extensions(input);
+                (tmp.0.len(), tmp.1.len())
+            };
+            assert_eq!(result, expected);
+        }
+    }
+}
